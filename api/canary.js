37a22eb0a71dbfbb8fc37acf3b798d4d540e1d01
NorthGateFunds <team@northgatefunds.com>
2026-01-14 22:27:33 -0700

    chore: add vercel api canary

---
+++ api/canary.js
@@ -0,0 +1,9 @@
+// api/canary.js
+export const config = { runtime: "nodejs" };
+
+export default function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader("content-type", "application/json; charset=utf-8");
+  res.setHeader("x-fw-canary", "api/canary.js@2026-01-15T05:30Z");
+  res.end(JSON.stringify({ ok: true, where: "api/canary.js" }));
+}
