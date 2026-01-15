921cd5c497c03228f76deef994252326b3d4a7a5
NorthGateFunds <team@northgatefunds.com>
2026-01-15 11:57:36 -0700

    chore: remove unused pages/ api (Vercel uses /api)

--- pages/api/_canary.js
+++
@@ -1,9 +0,0 @@
-// pages/api/canary.js
-export const config = { runtime: "nodejs" };
-
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader("content-type", "application/json; charset=utf-8");
-  res.setHeader("x-fw-canary", "pages/api/canary.js@2026-01-15T05:30Z");
-  res.end(JSON.stringify({ ok: true, where: "pages/api/canary.js" }));
-}
